a778286e996c6a3991ea6ae1bb8d4ae35a5c9e1a
Steven Xie <xies1999@hotmail.com>
2024-02-27 16:30:49 -0500

    updated class Casino, playGame property functioning as intended

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 class Casino {
+    constructor(name){
+        this.name = name;
+        this.timesPlayed = 0;
+    }
+    playGame(betAmount){
+        if(Math.random()<=0.5){
+            console.log(`${this.name} wins!`)
+        } else {
+            console.log(`Player wins ${betAmount*(this.timesPlayed + 1)} dollars!`)
+        }
+        this.timesPlayed += 1 
+        // console.log(this.timesPlayed)
+    }
     // Write code here
 };
 
