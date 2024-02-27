045b67b04a9ad5d4448def857ca19f3a2bf51eb7
Steven Xie <xies1999@hotmail.com>
2024-02-27 16:31:18 -0500

    removed unnecessary comments

--- app.js
+++ app.js
@@ -23,21 +23,4 @@ myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
 
-// BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
-console.log(myBonusCasino);
-myBonusCasino.playGame(5);
-myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
-myBonusCasino.playGame(35);
-*/
 
-// Extra BONUS TESTS
-/*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
-console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
